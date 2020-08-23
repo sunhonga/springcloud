b045f229de38a7f04526a0eca436939ef52170a0
1341484113@qq.com <1341484113@qq.com>
2020-08-23 21:55:45 +0800

    添加gateway

--- springcloud-order/src/main/java/com/sun/springcloudorder/controller/OrderController.java
+++ springcloud-order/src/main/java/com/sun/springcloudorder/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.sun.springcloudorder.controller;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,5 +22,4 @@ public class OrderController {
     String addOrder(){
         return port+"add orderSUCCESS";
     }
-
 }
