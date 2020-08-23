ed348bfdc03c1e30fc0c7a465245cf655f6af2e1
1341484113@qq.com <1341484113@qq.com>
2020-08-23 22:47:06 +0800

    添加gateway

---
+++ springcloud-gateway/src/main/java/com/sun/springcloudgateway/AuthRoutePredicateFactory.java
@@ -0,0 +1,46 @@
+package com.sun.springcloudgateway;
+
+import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class AuthRoutePredicateFactory extends AbstractRoutePredicateFactory<AuthRoutePredicateFactory.Config> {
+
+    public AuthRoutePredicateFactory(){
+        super(Config.class);
+    }
+
+    private static final String NAME_KEY="name";
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList(NAME_KEY);
+    }
+
+    @Override
+    public Predicate<ServerWebExchange> apply(Config config) {
+        return (exchange ->{
+            HttpHeaders headers = exchange.getRequest().getHeaders();
+            List<String> list = headers.get(config.getName());
+            return list.size()>0;
+        });
+    }
+
+    public static class Config{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
