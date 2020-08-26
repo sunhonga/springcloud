2bdb5cc227d71b5a3104aa58985716f4cfdeac67
1341484113@qq.com <1341484113@qq.com>
2020-08-26 22:31:11 +0800

    添加gateway

---
+++ springcloud-gateway/src/main/java/com/sun/springcloudgateway/IpAddressKeyResolver.java
@@ -0,0 +1,14 @@
+package com.sun.springcloudgateway;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class IpAddressKeyResolver implements KeyResolver  {
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+    }
+}
